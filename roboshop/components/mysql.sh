5d2691a8954bae450bb6f0aeb82b07a42ad74d23
CodingManoj <mvp2612@hotmail.com>
2022-12-29 22:34:23 +0530

    Correcting the function

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
+#!/bin/bash 
+
+COMPONENT=mysql
 
 source components/common.sh
-COMPONENT=mysql
 
 read -p 'Enter MySQL Password you wish to configure:' MYSQL_PWD
 
-
-echo -n  "Configuring $COMPONENT repo"
-curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/$COMPONENT.repo &>> $LOGFILE 
-stat $?
+echo -n "Configuring the $COMPONENT Repo:"
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo &>> $LOGFILE 
+stat $? 
 
 echo -n "Installing $COMPONENT:"
 yum install mysql-community-server -y &>> $LOGFILE 
@@ -21,22 +21,21 @@ stat $?
 echo -n "Changing the default password:"
 DEF_ROOT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk -F ' ' '{print $NF}')
 
+# I only want to change the default password only for the first time.
+# How do I come to know whether the password is changed or not.
+echo show databases | mysql -uroot -p${MYSQL_PWD} &>> $LOGFILE 
+if [ $? -ne 0 ] ; then 
+    echo -n "Reset Root Password"
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PWD}';" | mysql  --connect-expired-password  -uroot -p"${DEF_ROOT_PASSWORD}" &>> $LOGFILE 
+    stat $? 
+fi 
 
-echo show databases | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} &>> $LOGFILE 
-if [ $? -ne 0 ]
-then
-  echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PWD}';" > /tmp/root-pass-sql
-  DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
-  cat /tmp/root-pass-sql  | mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}" &>> $LOGFILE 
-  stat $? 
-fi
-
-echo -e "Removing Password Validate Plugin : "
-echo "show plugins" |  mysql -uroot -p${MYSQL_PWD} | grep validate_password &>> $LOGFILE 
-if [ $? -eq 0 ]; then
-  echo " uninstall plugin validate_password;" | mysql -uroot -p${MYSQL_PWD}   &>> $LOGFILE 
-  stat $? 
-fi
+echo show plugins | mysql -uroot -p${MYSQL_PWD} | grep validate_password; &>> $LOGFILE 
+if [ $? -eq 0 ] ; then 
+    echo -n "Uninstalling Password Validate Plugin: "
+    echo "uninstall plugin validate_password;" | mysql -uroot -p${MYSQL_PWD} &>> $LOGFILE 
+    stat $? 
+fi 
 
 echo -n "Downloading the $COMPONENT Schema:"
 cd /tmp 
@@ -44,12 +43,9 @@ curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/$COMPONENT/
 unzip -o $COMPONENT.zip &>> $LOGFILE
 stat $?
 
-cd $COMPONENT-main &>>$LOG
-
 echo -n "Injecting the $COMPONENT Schema:"
-mysql -uroot -p${MYSQL_PWD} <shipping.sql &>> $LOGFILE 
+cd /tmp/$COMPONENT-main/
+mysql -uroot -p${MYSQL_PWD} < shipping.sql &>> $LOGFILE
 stat $? 
 
 echo -e "\e[32m __________ $COMPONENT Installation Completed _________ \e[0m"
-
-
